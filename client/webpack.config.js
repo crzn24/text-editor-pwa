ec6730961dc46e4b689a450e02625e71fb67b4cd
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-02-19 16:30:42 -0800

    Removes . from before / on line 41 and 42

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -38,8 +38,8 @@ module.exports = () => {
         description: 'A text editor that runs in the browser! Take notes with with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
